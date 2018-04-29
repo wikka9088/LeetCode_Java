cf53ccf217536daff2c204daf507240a8390b54f
V+ <wikka9088@gmail.com>
2018-04-29 16:32:24 -0400

    Array_Partition_I

---
+++ LinkedList/src/Palindrome_Linked_List_easy.java
@@ -0,0 +1,18 @@
+
+public class Palindrome_Linked_List_easy {
+
+	public static void main(String[] args) {
+		/**
+		 * Given a singly linked list, determine if it is a palindrome.
+		 */
+		//follow up: Could you do it in O(n) time and O(1) space?
+		
+		
+
+	}
+	
+    public boolean isPalindrome(ListNode head) {
+        
+    }
+
+}
